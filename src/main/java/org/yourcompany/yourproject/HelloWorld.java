c7a45cf12634d78e1526797749b604fcf6e8d29d
Camila <camilacamapgnolo.s@gmail.com>
2024-06-30 13:29:02 -0300

    Adicionando comentarios

---
+++ src/main/java/org/yourcompany/yourproject/HelloWorld.java
@@ -0,0 +1,14 @@
+package org.yourcompany.yourproject;
+
+/**
+ *
+ * @author Camila Campagnolo
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        // Saida de dados para o console
+        System.out.println("Hello World!");
+    }
+    
+}
